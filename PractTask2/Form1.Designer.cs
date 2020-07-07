7bf37403e9bca350f9977315ee379b7a1ac318fc
Metlaski <46136189+Metlaski@users.noreply.github.com>
2020-07-07 22:12:04 +0300

    Добавьте файлы проекта.

---
+++ PractTask2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PractTask2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelLevel = new System.Windows.Forms.Label();
+            this.numericUpDownLevel = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownExtra = new System.Windows.Forms.NumericUpDown();
+            this.labelExtra = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLevel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownExtra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Location = new System.Drawing.Point(518, 9);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(117, 13);
+            this.labelLevel.TabIndex = 1;
+            this.labelLevel.Text = "Уровень прорисовки:";
+            // 
+            // numericUpDownLevel
+            // 
+            this.numericUpDownLevel.Location = new System.Drawing.Point(521, 25);
+            this.numericUpDownLevel.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownLevel.Name = "numericUpDownLevel";
+            this.numericUpDownLevel.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownLevel.TabIndex = 2;
+            this.numericUpDownLevel.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownLevel.ValueChanged += new System.EventHandler(this.numericUpDownLevel_ValueChanged);
+            // 
+            // numericUpDownExtra
+            // 
+            this.numericUpDownExtra.Location = new System.Drawing.Point(521, 64);
+            this.numericUpDownExtra.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numericUpDownExtra.Name = "numericUpDownExtra";
+            this.numericUpDownExtra.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownExtra.TabIndex = 4;
+            this.numericUpDownExtra.ValueChanged += new System.EventHandler(this.numericUpDownExtra_ValueChanged);
+            // 
+            // labelExtra
+            // 
+            this.labelExtra.AutoSize = true;
+            this.labelExtra.Location = new System.Drawing.Point(518, 48);
+            this.labelExtra.Name = "labelExtra";
+            this.labelExtra.Size = new System.Drawing.Size(200, 13);
+            this.labelExtra.TabIndex = 3;
+            this.labelExtra.Text = "Количество дополнительных уровней:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 524);
+            this.Controls.Add(this.numericUpDownExtra);
+            this.Controls.Add(this.labelExtra);
+            this.Controls.Add(this.numericUpDownLevel);
+            this.Controls.Add(this.labelLevel);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLevel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownExtra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelLevel;
+        private System.Windows.Forms.NumericUpDown numericUpDownLevel;
+        private System.Windows.Forms.NumericUpDown numericUpDownExtra;
+        private System.Windows.Forms.Label labelExtra;
+    }
+}
+
---
+++ PractTask2/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PractTask2
+{
+    public partial class Form1 : Form
+    {
+        private Tree fract = new Tree(new PointF(180, 250), new PointF(320, 250));
+
+        public class Tree
+        {
+            public class Node
+            {
+                public PointF dot1;
+                public PointF dot2;
+                public int level;
+                public Node left;
+                public Node right;
+            }
+
+            private List<Color> colors = new List<Color>();
+            private int maxLevel;
+            private Node root;
+
+            public Tree(PointF dot1, PointF dot2)
+            {
+                Node tempN = new Node();
+                root = tempN;
+                maxLevel = 0;
+                root.dot1 = dot1;
+                root.dot2 = dot2;
+                Random rand = new Random();
+                int R = rand.Next(0, 255);
+                int G = rand.Next(0, 255);
+                int B = rand.Next(0, 255);
+                Color temp = Color.FromArgb(R, G, B);
+                colors.Add(temp);
+                root.level = 0;
+            }
+
+            public void drawLayer(int drawLevel, PaintEventArgs e)
+            {
+                if (drawLevel > maxLevel)
+                {
+                    throw new ArgumentOutOfRangeException("level", "This level doesn't exists");
+                }
+
+                drawLayer(root, drawLevel, e);
+            }
+
+            private void drawLayer(Node parent, int drawLevel, PaintEventArgs e)
+            {
+                if (drawLevel == 0)
+                {
+                    Pen pen = new Pen(colors[parent.level]);
+                    e.Graphics.DrawLine(pen, parent.dot1, parent.dot2);
+                }
+                else
+                {
+                    drawLayer(parent.left, drawLevel - 1, e);
+                    drawLayer(parent.right, drawLevel - 1, e);
+                }
+            }
+
+            public void buffMax(int newMax)
+            {
+                if (newMax > maxLevel)
+                {
+                    int toGo = maxLevel;
+
+                    Random rand = new Random();
+                    for (int i = maxLevel; i < newMax; ++i)
+                    {
+                        int R = rand.Next(0, 255);
+                        int G = rand.Next(0, 255);
+                        int B = rand.Next(0, 255);
+                        Color levelColor = Color.FromArgb(R, G, B);
+                        colors.Add(levelColor);
+                    }
+                    maxLevel = newMax;
+                    buffMax(root, newMax - 1, toGo);
+                }
+            }
+
+            private void buffMax(Node parent, int newMax, int toGo)
+            {
+                if (toGo == 0)
+                {
+                    Node leftTemp = new Node();
+                    Node rightTemp = new Node();
+
+                    float newX = (parent.dot1.X + parent.dot2.X) / 2 - (parent.dot2.Y - parent.dot1.Y) / 2;
+                    float newY = (parent.dot1.Y + parent.dot2.Y) / 2 + (parent.dot2.X - parent.dot1.X) / 2;
+
+                    leftTemp.dot1 = parent.dot1;
+                    leftTemp.dot2.X = newX;
+                    leftTemp.dot2.Y = newY;
+
+                    rightTemp.dot1.X = newX;
+                    rightTemp.dot1.Y = newY;
+                    rightTemp.dot2 = parent.dot2;
+
+                    leftTemp.level = maxLevel - newMax;
+                    rightTemp.level = maxLevel - newMax;
+                    parent.left = leftTemp;
+                    parent.right = rightTemp;
+                }
+
+                if (newMax > 0)
+                {
+                    --newMax;
+                    if (toGo > 0)
+                        --toGo;
+                    buffMax(parent.left, newMax, toGo);
+                    buffMax(parent.right, newMax, toGo);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDownLevel_ValueChanged(object sender, EventArgs e)
+        {
+            fract.buffMax((int)numericUpDownLevel.Value - 1);
+            numericUpDownExtra.Maximum = numericUpDownLevel.Value - 1;
+            pictureBox.Refresh();
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(0, pictureBox.Height);
+            e.Graphics.ScaleTransform(1, -1);
+            fract.drawLayer((int)numericUpDownLevel.Value - 1, e);
+            if (numericUpDownExtra.Value > 0)
+                for (int i = 0; i < numericUpDownExtra.Value; ++i)
+                    fract.drawLayer((int)numericUpDownLevel.Value - 2 - i, e);
+        }
+
+        private void numericUpDownExtra_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox.Refresh();
+        }
+    }
+}
